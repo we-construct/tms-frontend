cf225d663533c4ebf2f3e72baaf5c2f8933e4245
DavidKizinger <karapetyand91@gmail.com>
2020-08-20 12:43:36 +0400

    added nav-bar

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,10 +1,11 @@
+import Navbar from "../Components/NavBar/NavBar";
 
 const routes = [
-//   {
-//     path: "/",
-//     exact: true,
-//     component: App,
-//   },
+    {
+        path: "/",
+        exact: true,
+        component: Navbar,
+    },
 ];
 
 export default routes;
