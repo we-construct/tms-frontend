5f5ceba74bd0d3af01322e965555c9c036c2597e
Vlad <vaarsenyan@gmail.com>
2020-08-25 01:56:01 +0400

    Add snackbars and notistack package

--- src/Containers/Login/index.jsx
+++ src/Containers/Login/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { loginUser } from "../../Redux/Users/actions";
+import { useSnackbar } from "notistack";
 import {
   Container,
   Typography,
@@ -16,17 +17,36 @@ import { NavLink } from "react-router-dom";
 import "./index.scss";
 
 const Login = ({ error, success, loginUser }) => {
+  const { enqueueSnackbar } = useSnackbar();
   const [check, setCheck] = useState(false);
   const [data, setData] = useState({
     email: "",
     password: "",
   });
 
+  useEffect(() => {
+    if (success !== null) {
+      enqueueSnackbar(
+        `${success.firstName}, thanks for accepting invitation.`,
+        {
+          variant: "success",
+        }
+      );
+    }
+    //eslint-disable-next-line
+  }, [success]);
+  useEffect(() => {
+    check ? enqueueSnackbar('I\'ll remember you!', {variant: 'info'}) : enqueueSnackbar('I will not remember you!', {variant: 'warning'});
+    //eslint-disable-next-line
+  }, [check]);
+
   const changeHandler = (e) => {
     const { name, value } = e.target;
     setData({ ...data, [name]: value });
   };
   const loginHandler = () => {
+    if (data.email === "" || data.password === "")
+      return enqueueSnackbar("Enter all fields", { variant: "error" });
     loginUser({
       email: data.email,
       password: data.password,
@@ -34,22 +54,16 @@ const Login = ({ error, success, loginUser }) => {
     });
   };
 
+
   return (
     <div className="loginPage">
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className="paper">
           <Avatar className="avatar" />
-          {success === null ? (
-            <Typography component="h1" variant="h5">
-              Sign In
-            </Typography>
-          ) : (
-            <Typography component="h5" variant="body1" color="primary" align="center">
-              Than you <span className="accepted_user">{success.firstName}</span> for accepting invitation, plaease
-              sign in!
-            </Typography>
-          )}
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
           <div className="signInForm">
             <TextField
               variant="outlined"
@@ -80,9 +94,9 @@ const Login = ({ error, success, loginUser }) => {
               helperText={error !== null ? error : null}
             />
             <FormControlLabel
-              onChange={() => setCheck(!check)}
               control={
                 <Checkbox
+                  onChange={() => setCheck(!check)}
                   value="remember"
                   color="primary"
                   name="remember"
