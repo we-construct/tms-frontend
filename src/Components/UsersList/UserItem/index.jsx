9a6959f871a8605f4de9e91fd22b87bcbc128e88
Vlad <vaarsenyan@gmail.com>
2020-08-26 11:27:37 +0400

    Fixed pagination

--- src/Components/UsersList/UserItem/index.jsx
+++ src/Components/UsersList/UserItem/index.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
 import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import Button from "@material-ui/core/Button";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
-const UserItem = ({ row }) => {
+const UserItem = ({ user }) => {
   const [open, setOpen] = useState(false);
+  const date = new Date(user.created_at);
+  const createdAt = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
   return (
     <>
@@ -24,46 +25,43 @@ const UserItem = ({ row }) => {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell align="left">{row.id}</TableCell>
+        <TableCell align="left">{user.id}</TableCell>
         <TableCell align="left">
-          {row.firstName} {row.lastName}
+          {user.first_name} {user.last_name}
         </TableCell>
-        <TableCell align="left">{row.email}</TableCell>
-        {row.role === 'Admin' ? (
-          <TableCell align="left" style={{color: '#4caf50',}}>{row.role}</TableCell>
+        <TableCell align="left">{user.email}</TableCell>
+        {user.role === 'Admin' ? (
+          <TableCell align="left" style={{color: '#4caf50',}}>{user.role}</TableCell>
         ):
         (
-          <TableCell align="left">{row.role}</TableCell>
+          <TableCell align="left">{user.role}</TableCell>
         )}
-        <TableCell align="left">{row.position}</TableCell>
-        <TableCell align="left">{row.createdAt}</TableCell>
-        <TableCell align="left">{row.createdBy}</TableCell>
-        {row.status === 'Active' ? (
-          <TableCell align="left" style={{color: '#4caf50',}}>{row.status}</TableCell>
-        ): row.status === 'Vacation' ?
+        <TableCell align="left">{user.position}</TableCell>
+        <TableCell align="left">{createdAt}</TableCell>
+        <TableCell align="left">{user.created_by_id}</TableCell>
+        {user.status === 'Active' ? (
+          <TableCell align="left" style={{color: '#4caf50',}}>{user.status}</TableCell>
+        ): user.status === 'Vacation' ?
         (
-          <TableCell align="left" style={{color: '#ffc107',}}>{row.status}</TableCell>
+          <TableCell align="left" style={{color: '#ffc107',}}>{user.status}</TableCell>
           ) : 
         (
-          <TableCell align="left" style={{color: '#ff1744',}}>{row.status}</TableCell>
+          <TableCell align="left" style={{color: '#ff1744',}}>{user.status}</TableCell>
         )}      
         </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                Actions
-              </Typography>
-              {row.status === 'Deactivated' ? (
-                <Button variant="contained" size="small" color="primary" style={{marginRight: '8px',}}>Activate</Button>
+              {user.status === 'Deactivated' ? (
+                <Button variant="contained" size="small" color="primary" style={{marginRight: '8px',}}>Activate {user.first_name}</Button>
               ):
               (
-                <Button variant="outlined" size="small" color="secondary" style={{marginRight: '8px',}}>Deactivate</Button>
+                <Button variant="outlined" size="small" color="secondary" style={{marginRight: '8px',}}>Deactivate {user.first_name}</Button>
               )
               }
-              <Button variant="outlined" size="small" color="secondary" style={{marginRight: '8px',}}>Delete</Button>
-              <Button variant="outlined" size="small" color="primary" style={{marginRight: '8px',}}>Edit</Button>
+              <Button variant="outlined" size="small" color="secondary" style={{marginRight: '8px',}}>Delete {user.first_name}</Button>
+              <Button variant="outlined" size="small" color="primary" style={{marginRight: '8px',}}>Edit {user.first_name}</Button>
             </Box>
           </Collapse>
         </TableCell>
--- src/Components/UsersList/index.jsx
+++ src/Components/UsersList/index.jsx
@@ -1,88 +1,98 @@
-import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TablePagination from '@material-ui/core/TablePagination';
-import UserItem from './UserItem';
+import React, { useState, useEffect } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Pagination from "@material-ui/lab/Pagination";
+import axios from "axios";
+import UserItem from "./UserItem";
 
-const rows = [
+const users = [
   {
     id: 1,
-    firstName: 'Vladimir',
-    lastName: 'Arsenyan',
-    email: 'vaarsenyan@gmail.com',
-    role: 'Admin',
-    position: 'Full Stack Developer',
-    status: 'Active',
-    createdBy: 'Vladimir',
-    createdAt: '2020/25/08',
+    firstName: "Vladimir",
+    lastName: "Arsenyan",
+    email: "vaarsenyan@gmail.com",
+    role: "Admin",
+    position: "Full Stack Developer",
+    status: "Active",
+    createdBy: "Vladimir",
+    createdAt: "2020/25/08",
   },
   {
     id: 2,
-    firstName: 'Test',
-    lastName: 'Deactivateyan',
-    email: 'test@gmail.com',
-    role: 'User',
-    position: 'HR',
-    status: 'Deactivated',
-    createdBy: 'Vladimir',
-    createdAt: '2020/25/08',
+    firstName: "Test",
+    lastName: "Deactivateyan",
+    email: "test@gmail.com",
+    role: "User",
+    position: "HR",
+    status: "Deactivated",
+    createdBy: "Vladimir",
+    createdAt: "2020/25/08",
   },
   {
     id: 3,
-    firstName: 'Test',
-    lastName: 'Vacation',
-    email: 'test2@gmail.com',
-    role: 'User',
-    position: 'PM',
-    status: 'Vacation',
-    createdBy: 'Vladimir',
-    createdAt: '2020/25/08',
+    firstName: "Test",
+    lastName: "Vacation",
+    email: "test2@gmail.com",
+    role: "User",
+    position: "PM",
+    status: "Vacation",
+    createdBy: "Vladimir",
+    createdAt: "2020/25/08",
   },
 ];
 
 const UsersList = () => {
-  // const [page, setPage] = React.useState(0);
-  const page = 0;
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState();
+
+  // placeholder for redux
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/get/users", {
+        accessToken:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM4Iiwicm9sZSI6MSwic3RhdHVzIjoxLCJlbWFpbCI6InZhYXJzZW55YW5AZ21haWwuY29tIiwiaWF0IjoxNTk4Mzg0MjI2fQ.TBIUwWxx2N3vQsS3Rb96mxh1xGSyBYribxd2qjAqbu8",
+      })
+      .then((res) => setData(res.data));
+  }, []);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
     <>
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell align="left">ID</TableCell>
-            <TableCell align="left">Username</TableCell>
-            <TableCell align="left">Email</TableCell>
-            <TableCell align="left">Role</TableCell>
-            <TableCell align="left">Position</TableCell>
-            <TableCell align="left">Created At</TableCell>
-            <TableCell align="left">Created By</TableCell>
-            <TableCell align="left">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <UserItem key={row.id} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    <TablePagination
-          rowsPerPageOptions={[]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={5}
-          page={page}
-          onChangePage={() => console.log('Need to fix')}
-        />
-    </TableContainer>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell align="left">ID</TableCell>
+              <TableCell align="left">Username</TableCell>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="left">Role</TableCell>
+              <TableCell align="left">Position</TableCell>
+              <TableCell align="left">Created At</TableCell>
+              <TableCell align="left">Created By</TableCell>
+              <TableCell align="left">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data === undefined
+              ? null
+              : data.users.map((user) => (
+                  <UserItem key={user.id} user={user} />
+            ))}
+          </TableBody>
+        </Table>
+        <Pagination count={users.length} page={page} onChange={handleChange} />
+      </TableContainer>
     </>
   );
-}
+};
 
 export default UsersList;
