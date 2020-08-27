ecf9b06b8d87cb5955e3f1aa431c76fc5e949c3e
AraMatevosyan777 <60426627+AraMatevosyan777@users.noreply.github.com>
2020-08-27 11:47:12 +0400

    Delete unnecessary files

--- src/Routes/publicRoutes.js
+++ src/Routes/publicRoutes.js
@@ -1,11 +1,17 @@
 import Login from '../Containers/Login';
 import AcceptInvitation from '../Containers/AcceptInvitation';
+import ResetPassword from '../Containers/ResetPassword';
 
 const publicRoutes = [
   {
     path: '/',
     exact: true,
     component: Login,
+  },
+  {
+    path: '/reset-password',
+    exact: true,
+    component: ResetPassword,
   },
   {
     path: '/invitation/:token',
